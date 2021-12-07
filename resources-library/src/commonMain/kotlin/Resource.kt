2e424e13cbf6c762d93285a350bfb82037594c4a
Gonçalo Silva <goncalossilva@gmail.com>
2021-12-07 00:26:47 +0000

    Adds docs to Resource

--- resources-library/src/commonMain/kotlin/Resource.kt
+++ resources-library/src/commonMain/kotlin/Resource.kt
@@ -1,6 +1,21 @@
 package com.goncalossilva.resource
 
+/**
+ * Provides access to resource on [path].
+ *
+ * The path should be relative to the project's directory, such as
+ * `src/commonTest/resources/some/optional/folders/file.txt`.
+ */
 expect public class Resource(path: String) {
+    /**
+     * Returns true when the resource exists, false when it doesn't.
+     */
     public fun exists(): Boolean
+
+    /**
+     * Returns the resource's content as a string.
+     *
+     * @throws RuntimeException when the resource doesn't exist or can't be read.
+     */
     public fun readText(): String
 }

a835a5286d908b5bb843fb16eb04a6a50801809e
Gonçalo Silva <goncalossilva@gmail.com>
2024-04-28 01:02:03 +0100

    Re-add watchosX64 build target
    Closes #110

--- resources-library/build.gradle.kts
+++ resources-library/build.gradle.kts
@@ -40,6 +40,7 @@ kotlin {
     iosSimulatorArm64()
     watchosArm32()
     watchosArm64()
+    watchosX64()
     watchosSimulatorArm64()
     tvosArm64()
     tvosX64()
--- resources-test/build.gradle.kts
+++ resources-test/build.gradle.kts
@@ -51,6 +51,7 @@ kotlin {
     iosSimulatorArm64()
     watchosArm32()
     watchosArm64()
+    watchosX64()
     watchosSimulatorArm64()
     tvosArm64()
     tvosX64()

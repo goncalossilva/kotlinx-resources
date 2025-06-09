313ecc9fc524f7ec0e729002d57c15afd61ba150
Urbanovich, Yahor <Yahor.Urbanovich@externals.adidas.com>
2025-06-09 23:47:58 +0300

    Remove test resource path prefix, update jvm resource load

--- resources-library/src/jvmMain/kotlin/Resource.kt
+++ resources-library/src/jvmMain/kotlin/Resource.kt
@@ -3,18 +3,23 @@ package com.goncalossilva.resources
 import java.io.File
 
 public actual class Resource actual constructor(private val path: String) {
-    private val file = File(path)
 
-    public actual fun exists(): Boolean = file.exists()
+    private val resourcePath: String?
+        get() = Resource::class.java.classLoader.getResource(path)?.path
+
+    private val resourceFile: File
+        get() = File(resourcePath)
+
+    public actual fun exists(): Boolean = resourcePath != null
 
     public actual fun readText(): String = runCatching {
-        file.readText()
+        resourceFile.readText()
     }.getOrElse { cause ->
         throw FileReadException("$path: No such file or directory", cause)
     }
 
     public actual fun readBytes(): ByteArray = runCatching {
-        file.readBytes()
+        resourceFile.readBytes()
     }.getOrElse { cause ->
         throw FileReadException("$path: No such file or directory", cause)
     }

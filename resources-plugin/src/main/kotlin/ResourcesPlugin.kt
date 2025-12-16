6d69d4f7a634f1262b63e3334d8d5ddb8ddd9086
Gonçalo Silva <goncalossilva@gmail.com>
2025-12-16 16:01:53 +0000

    Prefix Karma proxy config fragments

--- resources-plugin/src/main/kotlin/ResourcesPlugin.kt
+++ resources-plugin/src/main/kotlin/ResourcesPlugin.kt
@@ -104,7 +104,6 @@ class ResourcesPlugin : KotlinCompilerPluginSupportPlugin {
                     taskName = getTaskName(targetName, "proxyResources"),
                     // Task where karma.conf.js is created, in KotlinKarma.createTestExecutionSpec.
                     mustRunAfterTask = kotlinCompilation.processResourcesTaskName,
-                    dependantTask = kotlinCompilation.compileKotlinTaskName
                 )
             }
         }
@@ -184,7 +183,6 @@ class ResourcesPlugin : KotlinCompilerPluginSupportPlugin {
         kotlinCompilation: KotlinCompilation<*>,
         taskName: String,
         mustRunAfterTask: String,
-        dependantTask: String
     ): TaskProvider<Task> {
         val project = kotlinCompilation.target.project
         val tasks = project.tasks
@@ -192,7 +190,7 @@ class ResourcesPlugin : KotlinCompilerPluginSupportPlugin {
             .resolve("karma.config.d")
             .apply { mkdirs() }
             // Avoid cleanup races between multiple browser targets (e.g., js/wasmJs).
-            .resolve("$taskName.js")
+            .resolve("resources-$taskName.js")
 
         val proxyResourcesTask = tasks.register(taskName) { task ->
             @Suppress("ObjectLiteralToLambda")
@@ -228,15 +226,13 @@ class ResourcesPlugin : KotlinCompilerPluginSupportPlugin {
             })
             task.mustRunAfter(mustRunAfterTask)
         }
-        tasks.named(dependantTask).configure {
-            it.dependsOn(proxyResourcesTask)
-        }
 
         val cleanupConfFileTask = tasks.register("${taskName}Cleanup", Delete::class.java) {
             it.delete = setOf(confFile)
         }
-        tasks.named(getTaskName(kotlinCompilation.target.name, "browser", kotlinCompilation.name)) {
-            it.finalizedBy(cleanupConfFileTask)
+        tasks.named(getTaskName(kotlinCompilation.target.name, "browser", kotlinCompilation.name)) { browserTestTask ->
+            browserTestTask.dependsOn(proxyResourcesTask)
+            browserTestTask.finalizedBy(cleanupConfFileTask)
         }
 
         return proxyResourcesTask

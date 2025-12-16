48c2163cb7ec03aa3ca99b48ca343719f7490b86
Gonçalo Silva <goncalossilva@gmail.com>
2025-12-16 15:30:12 +0000

    Encapsulate Wasm/JS resource implementations

--- resources-library/src/wasmJsMain/kotlin/Resource.kt
+++ resources-library/src/wasmJsMain/kotlin/Resource.kt
@@ -12,17 +12,17 @@ private val IS_BROWSER: Boolean = js(IS_BROWSER_JS_CHECK)
 private val IS_NODE: Boolean = js(IS_NODE_JS_CHECK)
 
 private fun nodeExistsSync(path: JsString): Boolean = js("require('fs').existsSync(path)")
+
 private fun nodeReadFileSync(path: JsString, encoding: JsString): JsString =
     js("require('fs').readFileSync(path, encoding)")
+
+private fun nodeReadFileSyncBytes(path: JsString): NodeBuffer =
+    js("require('fs').readFileSync(path)")
 
 private external class NodeBuffer : JsAny {
     val length: Int
     operator fun get(index: Int): Byte
 }
-
-private fun nodeReadFileSyncBytes(path: JsString): NodeBuffer = js("require('fs').readFileSync(path)")
-
-private fun NodeBuffer.toByteArray(): ByteArray = ByteArray(length) { this[it] }
 
 private external class XMLHttpRequest : JsAny {
     fun open(method: JsString, url: JsString, async: Boolean)
@@ -40,67 +40,82 @@ private fun createXMLHttpRequest(): XMLHttpRequest = js("new XMLHttpRequest()")
  * See: https://youtrack.jetbrains.com/issue/KT-47038
  */
 public actual class Resource actual constructor(private val path: String) {
+    private val resourceBrowser: ResourceBrowser by lazy { ResourceBrowser(path) }
+    private val resourceNode: ResourceNode by lazy { ResourceNode(path) }
+
     public actual fun exists(): Boolean = when {
-        IS_BROWSER -> existsBrowser()
-        IS_NODE -> existsNode()
+        IS_BROWSER -> resourceBrowser.exists()
+        IS_NODE -> resourceNode.exists()
         else -> throw UnsupportedOperationException("Unsupported Wasm/JS runtime")
     }
 
     public actual fun readText(): String = when {
-        IS_BROWSER -> readTextBrowser()
-        IS_NODE -> readTextNode()
+        IS_BROWSER -> resourceBrowser.readText()
+        IS_NODE -> resourceNode.readText()
         else -> throw UnsupportedOperationException("Unsupported Wasm/JS runtime")
     }
 
     public actual fun readBytes(): ByteArray = when {
-        IS_BROWSER -> readBytesBrowser()
-        IS_NODE -> readBytesNode()
+        IS_BROWSER -> resourceBrowser.readBytes()
+        IS_NODE -> resourceNode.readBytes()
         else -> throw UnsupportedOperationException("Unsupported Wasm/JS runtime")
     }
 
-    private fun createRequest(config: (XMLHttpRequest.() -> Unit)? = null) = createXMLHttpRequest().apply {
-        open("GET".toJsString(), path.toJsString(), false)
-        config?.invoke(this)
-        send()
-    }
+    private class ResourceBrowser(path: String) {
+        private val jsPath: JsString = path.toJsString()
+        private val errorPrefix: String = path
 
-    @Suppress("MagicNumber")
-    private fun XMLHttpRequest.isSuccessful() = status in 200..299
+        fun exists(): Boolean = request().isSuccessful()
 
-    private fun existsBrowser(): Boolean = createRequest().isSuccessful()
-
-    private fun readTextBrowser(): String {
-        val request = createRequest()
-        return if (request.isSuccessful()) {
-            request.responseText.toString()
-        } else {
-            throw FileReadException("$path: Read failed: ${request.statusText}")
+        fun readText(): String {
+            val request = request()
+            return if (request.isSuccessful()) {
+                request.responseText.toString()
+            } else {
+                throw FileReadException("$errorPrefix: Read failed: ${request.statusText}")
+            }
         }
-    }
 
-    private fun readBytesBrowser(): ByteArray {
-        val request = createRequest {
-            overrideMimeType("text/plain; charset=x-user-defined".toJsString())
+        fun readBytes(): ByteArray {
+            val request = request {
+                overrideMimeType("text/plain; charset=x-user-defined".toJsString())
+            }
+            return if (request.isSuccessful()) {
+                val response = request.responseText.toString()
+                ByteArray(response.length) { response[it].code.toUByte().toByte() }
+            } else {
+                throw FileReadException("$errorPrefix: Read failed: ${request.statusText}")
+            }
         }
-        return if (request.isSuccessful()) {
-            val response = request.responseText.toString()
-            ByteArray(response.length) { response[it].code.toUByte().toByte() }
-        } else {
-            throw FileReadException("$path: Read failed: ${request.statusText}")
+
+        private fun request(config: (XMLHttpRequest.() -> Unit)? = null): XMLHttpRequest = createXMLHttpRequest().apply {
+            open("GET".toJsString(), jsPath, false)
+            config?.invoke(this)
+            send()
         }
+
+        @Suppress("MagicNumber")
+        private fun XMLHttpRequest.isSuccessful() = status in 200..299
     }
 
-    private fun existsNode(): Boolean = nodeExistsSync(path.toJsString())
+    private class ResourceNode(path: String) {
+        private val jsPath: JsString = path.toJsString()
+        private val errorPrefix: String = path
 
-    private fun readTextNode(): String = runCatching {
-        nodeReadFileSync(path.toJsString(), "utf8".toJsString()).toString()
-    }.getOrElse { cause ->
-        throw FileReadException("$path: Read failed", cause)
-    }
+        fun exists(): Boolean = nodeExistsSync(jsPath)
 
-    private fun readBytesNode(): ByteArray = runCatching {
-        nodeReadFileSyncBytes(path.toJsString()).toByteArray()
-    }.getOrElse { cause ->
-        throw FileReadException("$path: Read failed", cause)
+        fun readText(): String = runCatching {
+            nodeReadFileSync(jsPath, "utf8".toJsString()).toString()
+        }.getOrElse { cause ->
+            throw FileReadException("$errorPrefix: Read failed", cause)
+        }
+
+        fun readBytes(): ByteArray = runCatching {
+            nodeReadFileSyncBytes(jsPath).toByteArray()
+        }.getOrElse { cause ->
+            throw FileReadException("$errorPrefix: Read failed", cause)
+        }
+
+        private fun NodeBuffer.toByteArray(): ByteArray = ByteArray(length) { this[it] }
     }
 }
